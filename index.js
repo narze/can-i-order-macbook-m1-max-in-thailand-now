f7f861bd1b0cfa6bd075a339c2acbc2b1d424463
Manassarn Manoonchai <manassarn@gmail.com>
2021-10-29 23:24:32 +0700

    Use headless mode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer"
 ;(async () => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
 
   await page.goto(
